caa8e18c1bbc69fde42a207a400a8b51b126634a
Adam Hundley <adamhundley@gmail.com>
2016-03-19 19:51:11 -0400

    password reset complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.all.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
